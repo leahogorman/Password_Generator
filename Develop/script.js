a36f9f084ffc49232d105be17ff5a6fecf63ea0c
Leah O'Gorman <leah.p.ogorman@gmail.com>
2020-09-24 17:44:17 -0400

    worked on my javascript and readme files

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,59 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", startPrompt);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Character sets valid for password
 
-  passwordText.value = password;
+var numberCharacters = "0123456789"
+var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz"
+var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var symbolCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
+// Starter prompt
+// user enters number of characters
+// error statement if they enter less than 8 or more than 128
+function startPrompt(){
+  var allCharacters = "";
+  var characterNumber = prompt("How many characters would you like your password to contain?");
+  while (characterNumber<8 || characterNumber>128) {
+    alert("Password Length must be at least 8 characters and under 128")
+    var characterNumber = prompt("How many characters would you like your password to contain?");
+  } 
+
+
+// User chooses character sets
+// using if statements the user chooses what characters are part of the allCharacters string
+
+symbolQuestion = confirm("Click OK to confirm including Special Characters");
+numberQuestion = confirm("Click OK to confirm including Numeric Characters");
+lowerCaseQuestion = confirm("Click OK to confirm including Lowercase Characters");
+upperCaseQuestion = confirm("Click OK to confirm including Uppercase Characters");
+
+if (symbolQuestion===true) {
+  allCharacters = allCharacters.concat(symbolCharacters);
 }
+if (numberQuestion===true) {
+  allCharacters = allCharacters.concat(numberCharacters);
+}
+if (lowerCaseQuestion===true) {
+  allCharacters = allCharacters.concat(lowerCaseCharacters);
+}
+if (upperCaseQuestion===true) {
+  allCharacters = allCharacters.concat(upperCaseCharacters);
+}
+// GENERATE PASSWORD --
+//  randomizes the allCharacters string
+//  uses the randomized string to create a password
+//  generates password when button is clicked
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  var password = "";
+  for (var i=0; i<characterNumber; i++) {
+    var rnum = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters.substring(rnum, rnum+1);
+
+  }
+  var passwordBox = document.querySelector("#password")
+  passwordBox.value = password;
+}
+generatePassword();
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Password_Generator
+# Description
+    For this project we were asked to create a random password generator that would allow the User to choose the number of characters they wanted to use and the type of characters they'd like to use; and would then generate a random password for the User.
+# HTML and CSS
+    For this project the HTML and CSS were given to us from the start
+# Javascript
+    START PROMPT
+    For this project we had to connect the generateBtn to the prompt so that when a user clicked the button the prompt would appear. In order to do this, I created an event listener that on click would start the prompt
+    VARIABLE STRINGS
+    The User needed to be able to choose from different characters to decide what types of characters they wanted to use. In order to do this I created 4 strings for numbers, upper case characters, lower case characters, and symbols
+    OPTIONS
+    The User needed to be able to choose from different options to create their password. In order to do so I created different if statements for the different character strings and added these characters to the "allCharacters" string.
+    GENERATE
+    After the allCharacters string had the User's choice of characters the password then needed to randomize the choice. I did so using Math.floor(Math.random) to randomize the full integers from the allCharacters string.
+    SHOW PASSWORD
+    I then assigned the password to the id=password box from the index so that when the prompts were completed the password would show up in the correct spot.
+# Aids
+    This project was done with research via W3Schools and MDN WebDocs
+    https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+    https://www.w3schools.com/jsref/met_document_queryselector.asp
+# Collaborators
+    While this was done individually it was done with instruction from Fil and Daniel.
+    https://github.com/c0dehot
+    https://github.com/shibeknight
+    Also aid from my group.
+    Victor Scherman, Shaun Samoridny, Jessica Zonta, and Anjali Pant
+# Links
